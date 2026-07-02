f87550f6b8ed89a227d2d8b80f9ff1a19410cbd5
bearnannan <saweksoot@gmail.com>
2026-07-02 11:35:24 +0700

    Bump app version to 3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.universalwatermark"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 300
+        versionName = "3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
